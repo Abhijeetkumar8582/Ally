a173fbafa3d33999727c9feff861cbe352dd5c19
Abhijeet kumar <abhijeet122kumar@gmail.com>
2025-06-13 14:02:37 +0530

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,8 +7,9 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-          <script defer type="text/javascript" src="https://prod-us-druid-cdn.azureedge.net/v2/druid_webchat_v2_load.js?botId=87466e44-86a2-483b-ed4e-08dd8d8f8ad7&amp;baseUrl=https%3A%2F%2Fdruidapi.us.druidplatform.com">
+           <script defer type="text/javascript" src="https://Agents.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=0b00dbd9-73f8-40c9-3ff4-08dda434cab0&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
+
       </body>
     </Html>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>Clearme</title>
+      <title>CODA</title>
     </Head>
       <div>
         <h4></h4>
